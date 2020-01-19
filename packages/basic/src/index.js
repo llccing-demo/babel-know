d6ee04285b9e3312acc8ef522adc3c4c4cf0c575
liuchunfeng <liuchunfeng@didiglobal.com>
2020-01-19 12:56:07 +0800

    update 调整webpack.config.js，增加js loader

--- packages/basic/src/index.js
+++ packages/basic/src/index.js
@@ -1,9 +1,9 @@
-import _ from 'lodash'
+// import _ from 'lodash'
 
 function component() {
   const element = document.createElement('div')
 
-  element.innerHTML = _.join(['Hello', 'webpack'])
+  element.innerHTML = ['Hello', 'webpack'].join(',')
 
   return element
 }
--- packages/basic/webpack.config.js
+++ packages/basic/webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'main-babel.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      }
+    ]
   }
 }
